6c1adc31d279e7eee48c1776a7263c8992cde412
zmercury <messiniki@outlook.com>
2026-01-29 16:34:33 +0545

    feat: added forge

---
+++ forge.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    packagerConfig: {
+        name: 'ClipSnap',
+        executableName: 'ClipSnap',
+        asar: true,
+        icon: './icon' // Add icon path if you have one
+    },
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['win32'],
+            config: {}
+        }
+    ],
+    plugins: [],
+};
